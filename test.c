7a33a4064953331fa5b72c788ed2d8b8639ed554
nabbbbi <nabbbbi@mail.ru>
2022-06-04 16:16:51 +0300

    added

--- test.c
+++ test.c
@@ -1,38 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct item {
-  int data;
-  struct item *next;
-};
-
-struct item *frst(int *arr, int len)
+int main ()
 {
-  struct item *first=NULL, *last=NULL, *tmp;
-  int i;
-  for (i=0; i < len; i++)
-    {
-      tmp = malloc(sizeof(struct item));
-      tmp->data=arr[i];
-      tmp->next=NULL;
-      if (last)
-	{
-	  last->next=tmp;
-	  last=last->next;
-	}
-      else
-	{
-	  first=last=tmp;
-	}
-    }
-  return first;
-}
-
-int main()
-{
-  struct item *f;
-  int m[] = {3,5,5,2,5,1};
-  f = frst(m,sizeof(m));
-  printf("%d\n",f->data);
+  char str[32];
+  int n;
+  n=35;
+  sprintf(str,"%d", n);
+  printf("%s\n", str);
   return 0;
 }
