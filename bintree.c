07a71fc34d3bc5231764f90b3a6635dfc9e0d66f
nabbbbi <nabbbbi@mail.ru>
2022-06-08 02:29:40 +0300

    added

--- bintree.c
+++ bintree.c
@@ -26,9 +26,9 @@ void int_bin_tree_print_loop(struct node *r)
   struct backpath *bp, *tmp; /*tmp for create stack list*/
   bp=malloc(sizeof(*bp));
   bp->p=r; /*get from argument of function*/
+  bp->st=start;
   bp->next=NULL; /*the first node. we will emulate stack as in recursion 
 		   call*/
-  bp->st=start;
   while(bp)
     {
       switch(bp->st)
@@ -48,7 +48,7 @@ void int_bin_tree_print_loop(struct node *r)
 	    }
 	  /*no break here*/
 	case left_visited:
-	  printf("%d", bp->p->val);
+	  printf("%d\n", bp->p->val);
 	  bp->st=completed;
 	  if(bp->p->right)
 	    {
@@ -80,3 +80,45 @@ void int_bin_tree_print_rec(struct node *r)
 }
 */
 
+void int_bin_tree_add(struct node **root, int n) /*address of pointer*/
+{
+  if (!*root) /*only if node is empty*/
+    {
+      *root=malloc(sizeof(**root));
+      (*root)->val=n; 
+      (*root)->left=NULL; /*branchs will be empty*/
+      (*root)->right=NULL;
+      return;
+    }
+  if ((*root)->val == n) /*n is already exists. no need to add*/
+    return;
+  if(n < (*root)->val)
+    int_bin_tree_add(&(*root)->left,n); /*take address of left branch 
+					  of node*/
+  else
+    int_bin_tree_add(&(*root)->right,n); /*take address of right branch
+					   of node*/
+}
+void int_bin_tree_dispose(struct node *r)
+{
+  if (!r)
+    return;
+  int_bin_tree_dispose(r->left);
+  int_bin_tree_dispose(r->right);
+  free(r);
+}
+int main()
+{
+  static const int vals[] = {
+    50, 25, 75, 15, 30, 60, 90, 10, 20, 40,
+    60, 70, 80, 95, 5, 35, 45, 55, 65, 85
+  };
+  struct node *root=NULL; /*if not initialize this structure there will be Segmentation Fault*/
+  int i;
+  for (i=0;i < sizeof(vals) / sizeof(*vals); i++)
+    int_bin_tree_add(&root,vals[i]); /*add integers to binary tree*/
+  int_bin_tree_print_loop(root); /*print all elements in binary tree*/
+  int_bin_tree_dispose(root); /*delete all dynamic elements*/
+  printf("number of indexes in array: %ld\nsize of each index: %ld bytes\n",sizeof(vals), sizeof(*vals));
+  return 0;
+}
