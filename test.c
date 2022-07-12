0b303de1702eb0234352b241768ca6ac3b097e39
nabbbbi <nabbbbi@mail.ru>
2022-07-12 12:18:01 +0300

    added automatically

--- test.c
+++ test.c
@@ -7,17 +7,6 @@ struct node
   struct node *left, *right;
 };
 
-
-void read_int_in_bintree(struct node *r)
-{
-  if(!r)
-    {
-      read_int_in_bintree(r->left);
-      printf("%d",r->data);
-      read_int_in_bintree(r->right);
-    }
-}
-
 void add_int_to_bintree(struct node **root, int n)
 {
   if (!*root) 
@@ -34,6 +23,16 @@ void add_int_to_bintree(struct node **root, int n)
     add_int_to_bintree(&(*root)->left, n);
   else
     add_int_to_bintree(&(*root)->right, n);
+}
+
+void read_int_in_bintree(struct node *r)
+{
+  if(!r)
+    {
+      read_int_in_bintree(r->left);
+      printf("%d",r->data);
+      read_int_in_bintree(r->right);
+    }
 }
 
 int main()
