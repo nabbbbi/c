a422f6be79dc64854f7a64288bc34fb425b250dd
nabbbbi <nabbbbi@mail.ru>
2022-07-22 12:00:01 +0300

    added automatically

--- test2.c
+++ test2.c
@@ -5,57 +5,59 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <signal.h>
 
-enum {size_of_queue = 16};
-enum {port1 = 1024, port2 = 1025};
-char *message = "hello";
-char buf[10];
+void handler()
+{
+  printf("signal 'CTRL+C' received!\n");
+}
+
+char buf[150];
 
 int main()
 {
-  int ok, ls,cls,cl;
-  socklen_t len;
-  struct sockaddr_in addr;
-  ls = socket(AF_INET, SOCK_STREAM,0);
-  if(ls == -1)
+  fd_set input;
+  int result, len;
+  struct timeval t;
+  signal(SIGINT, handler);
+  for(;;)
     {
-      perror("socket");
-      return 1;
+      FD_ZERO(&input);
+      FD_SET(0, &input);
+      t.tv_sec=3;
+      t.tv_usec=500000;
+      result = select(FD_SETSIZE, &input,NULL, NULL, &t);
+      switch(result){
+      case(-1):
+	if (errno = EINTR)
+	  {
+	    printf("signal is received\n");
+	    return 0;
+	  }
+	perror("select");
+	exit(1);
+      case(0):
+      printf("timeout(-3.5s) -_-\n");
+      break;
+      default:
+	if(FD_ISSET(0, &input))
+	  {
+	    ioctl(0, FIONREAD,&len);
+	    if(len ==0)
+	      {
+		printf("keyboard donen. EOF\n"); /* we received eof signal */
+		exit(0);
+	      }
+	    buf[len]=0;
+	    read(0,buf,len);
+	    printf("%s",buf);
+	  }
+	break;
+      }
     }
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port1);
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  ok = bind(ls,(struct sockaddr*)&addr, sizeof(addr));
-  if(ok == -1)
-    {
-      perror("bind");
-      return 1;
-    }
-  listen(ls, size_of_queue);
-  cl = socket(AF_INET, SOCK_STREAM,0);
-  if (cl == -1)
-    {
-      perror("socket");
-      return 1;
-    }
-  ok = connect(cl,(struct sockaddr*)&addr, sizeof(addr));
-  if (ok == -1)
-    {
-      perror("connect");
-      return 1;
-    }
-  len = sizeof(addr);
-  cls = accept(ls,(struct sockaddr*)&addr,&len);
-  if (cls == -1)
-    {
-      perror("accept");
-      return 1;
-    }
-  write(cl, message, sizeof(message));
-  read(cls,buf,sizeof(buf));
-  write(1, buf, sizeof(buf));
-  write(1,"\n",1);
   return 0;
 }
-
-
+  
